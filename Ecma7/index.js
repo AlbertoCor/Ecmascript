153c6efa07de93b010b222ba3d12ca8381c1a2f1
Alberto Coronado <ac.insite15@live.com>
2021-08-16 17:22:41 -0500

    Update Readme file with Ecmascript V7 principal updates and adding examples in folder ecma7/index.js

---
+++ Ecma7/index.js
@@ -0,0 +1,19 @@
+
+// Use of include will be with an array or string to search an element on objective
+
+let numb = [1, 2, 3, 7, 8];
+
+if (numb.includes(5)){
+    console.log("Yes, array include number 7 !!")
+} else {
+    console.log("not found number in array.")
+}
+
+
+// Elevate a number
+
+let base = 4;
+let exponent = 3; 
+let result = base ** exponent;
+
+console.log(result);
